50537953950bf99623b5ae33572ee36790aa06db
Zain Javed <zjaved.bese17seecs@seecs.edu.pk>
2023-12-10 11:28:36 +0500

    add migrate

---
+++ controllers/migrate.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"chat-app/models"
+	"chat-app/utils"
+
+	"github.com/astaxie/beego"
+)
+
+type MigrationController struct {
+	beego.Controller
+}
+
+func (c *MigrationController) Post() {
+
+	dbKey := c.Ctx.Input.Header("Authorization")
+
+	envDBKey := utils.GoDotEnvVariable("DB_ACCESS_KEY")
+
+	if dbKey != envDBKey {
+		utils.CreateErrorResponse(&c.Controller, 422, "Invalid key")
+	}
+
+	db := utils.ConnectDB()
+	db.AutoMigrate(&models.User{})
+
+	responseData := map[string]interface{}{
+		"message": "Migration completed successfully!",
+	}
+
+	c.Data["json"] = responseData
+	c.ServeJSON()
+}
