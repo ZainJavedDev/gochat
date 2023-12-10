96544abf27f98a06ef289b040dc2096f6ba4c237
Zain Javed <zjaved.bese17seecs@seecs.edu.pk>
2023-12-10 11:32:26 +0500

    add routes and tested migration

--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,13 @@ package routers
 
 import (
 	"chat-app/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/migrate", &controllers.MigrationController{})
+	beego.Router("/signup", &controllers.SignupController{})
+	beego.Router("/login", &controllers.LoginController{})
 }
